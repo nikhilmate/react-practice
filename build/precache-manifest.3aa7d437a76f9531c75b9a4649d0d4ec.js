2f4c9e93a6ae3c6788f5943e40459b69a75ce882
nikhil.mate <nikhil.mate>
2019-07-08 01:34:26 +0530

    firsts

---
+++ build/precache-manifest.3aa7d437a76f9531c75b9a4649d0d4ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efd80884756819f366f75ec4e678b64",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "0771cc8062ba3fdf8634",
+    "url": "/react-practice/static/css/main.12b5f37d.chunk.css"
+  },
+  {
+    "revision": "d3a5e1539c6231972056",
+    "url": "/react-practice/static/js/2.ff80c9cf.chunk.js"
+  },
+  {
+    "revision": "0771cc8062ba3fdf8634",
+    "url": "/react-practice/static/js/main.7731feaa.chunk.js"
+  },
+  {
+    "revision": "80917382538bb08b9aec",
+    "url": "/react-practice/static/js/runtime~main.21a1c6d2.js"
+  }
+]);
